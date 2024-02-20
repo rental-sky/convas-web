0169b97a4762d9e8591b6371cec0dde56c94b2ad
ChristianSnchz <christsnchz@gmail.com>
2024-02-20 19:58:01 -0300

    meta

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -43,7 +43,7 @@ const Cart = () => {
       <SkeletonListContext.Provider
         value={{ xl: 14, lg: 24, md: 24, sm: 24, xs: 24 }}
       >
-        <MainLayout title={`Convans eCommerce - Cart`}>
+        <MainLayout title={`Covans Rental Snow - Cart`}>
           <Row className="cart-wrapper boxed-width">
             <Col xl={14} lg={24} md={24} sm={24} xs={24}>
               <CartListRenderer
